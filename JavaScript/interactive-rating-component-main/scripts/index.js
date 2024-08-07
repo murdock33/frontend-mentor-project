7c5c38ae10f1d52e6c179e720743719e8da01efa
murdock33 <tomhagen849@gmail.com>
2024-08-07 20:01:25 +0700

    Made several modifications to the HTML, CSS, and JavaScript code.

--- JavaScript/interactive-rating-component-main/scripts/index.js
+++ JavaScript/interactive-rating-component-main/scripts/index.js
@@ -1,38 +1,27 @@
-const ratingContainer = document.querySelector(".rating-container");
-let previousActiveBtn = null;
+const btnEl = document.querySelectorAll(".rating-btn");
+const submitBtn = document.querySelector(".submit-btn");
+let str = "";
+let value = 0;
 
-document.addEventListener("click", (event) => {
-  const target = event.target;
-  const isRatingButton = target.closest(".rating-container");
+btnEl.forEach((el) => {
+  el.addEventListener("click", (event) => {
+    const prevBtn = document.querySelector(".active-btn");
 
-  if (!isRatingButton) {
-    if (previousActiveBtn) {
-      previousActiveBtn.classList.remove("active-btn");
-      previousActiveBtn = null;
-    }
-    return;
-  }
-
-  if (target.nodeName.includes("BUTTON")) {
-    if (previousActiveBtn) {
-      previousActiveBtn.classList.remove("active-btn");
+    if (prevBtn) {
+      prevBtn.classList.remove("active-btn");
     }
 
-    target.classList.add("active-btn");
-    previousActiveBtn = target;
-  }
+    value = event.target.textContent;
+    event.target.classList.add("active-btn");
+  });
 });
 
-let str = "";
-function getRatingValue(value) {
-  str = `You selected ${value} out of 5`;
-}
+submitBtn.addEventListener("click", () => {
+  if (!value) return;
 
-function submitBtn() {
-  if (!str) return;
-  
+  str = `You selected ${value} out of 5`;
   document.querySelector(".result").innerHTML = str;
   document.querySelector(".front-face").classList.add("hidden-visibility");
   document.querySelector(".back-face").classList.remove("hidden-visibility");
   str = "";
-}
+});
